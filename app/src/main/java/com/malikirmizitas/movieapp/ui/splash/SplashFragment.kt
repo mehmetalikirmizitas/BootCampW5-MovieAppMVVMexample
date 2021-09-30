991737bde23f6f0279e6126100d0d4801e04bda9
kirmizitasmali <malikirmizitas@outlook.com>
2021-09-30 18:06:49 +0300

    Changed splash screen animation

--- app/src/main/java/com/malikirmizitas/movieapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/malikirmizitas/movieapp/ui/splash/SplashFragment.kt
@@ -3,29 +3,41 @@ package com.malikirmizitas.movieapp.ui.splash
 import android.animation.Animator
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.airbnb.lottie.LottieAnimationView
+import androidx.navigation.fragment.findNavController
 import com.malikirmizitas.movieapp.R
-import com.malikirmizitas.movieapp.utils.toastShort
+import com.malikirmizitas.movieapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //lottieListener()
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    private fun lottieListener() {
-        val lottie: LottieAnimationView = view?.findViewById(R.id.splash_anim)!!
-        lottie.addAnimatorListener(object : Animator.AnimatorListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lottieAnimation()
+    }
+
+    private fun lottieAnimation() {
+        binding.splashAnim.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animation: Animator?) {
                 Log.v("Animation", "Started")
             }
 
             override fun onAnimationEnd(animation: Animator?) {
-                toastShort("Animation End")
-                //findNavController().navigate(R.id.action_splashFragment_to_newsFragment)
+                findNavController().navigate(R.id.action_splashFragment_to_tabLayoutControllerFragment)
             }
 
             override fun onAnimationCancel(animation: Animator?) {
@@ -37,5 +49,4 @@ class SplashFragment : Fragment() {
             }
         })
     }
-
 }
